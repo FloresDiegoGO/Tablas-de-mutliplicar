f5166a2071cd3e5022d1cbfa3140630625b6c2d0
FloresDiegoGO <99056715+FloresDiegoGO@users.noreply.github.com>
2023-03-31 14:06:38 -0500

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,76 @@
+var Tabla2 = document.getElementById('Tabla-2')
+var Tabla3 = document.getElementById('Tabla-3')
+var Tabla4 = document.getElementById('Tabla-4')
+var Tabla5 = document.getElementById('Tabla-5')
+var Tabla6 = document.getElementById('Tabla-6')
+var Tabla7 = document.getElementById('Tabla-7')
+var Tabla8 = document.getElementById('Tabla-8')
+var Tabla9 = document.getElementById('Tabla-9')
+// Contadores
+
+var contador2 = 0;
+var contador3 = 0; 
+var contador4 = 0; 
+var contador5 = 0; 
+var contador6 = 0; 
+var contador7 = 0; 
+var contador8 = 0; 
+var contador9 = 0;  
+// Tabla del dos
+function TablaDelDos(){
+
+    Tabla2.innerHTML = '2 ' + ' X ' + contador2 + ' = ' + contador2 * 2;
+    Tabla2.style.backgroundColor = 'yellow';
+    contador2 ++
+
+}
+// Tabla del tres
+function TablaDelTres(){
+
+    Tabla3.innerHTML = '3' + ' X ' + contador3 + ' = ' + contador3 * 3;
+    Tabla3.style.backgroundColor = 'yellow';
+    contador3 ++
+}
+// Tabla del cuatro
+function TablaDelCuatro(){
+
+    Tabla4.innerHTML = '4' + ' X ' + contador4 + ' = ' + contador4 * 4;
+    Tabla4.style.backgroundColor = 'yellow';
+    contador4 ++
+}
+// Tabla del cinco
+function TablaDelCinco(){
+
+    Tabla5.innerHTML = '5' + ' X ' + contador5 + ' = ' + contador5 * 5;
+    Tabla5.style.backgroundColor = 'yellow';
+    contador5 ++
+}
+// Tabla del seis
+function TablaDelSeis(){
+
+    Tabla6.innerHTML = '6' + ' X ' + contador6 + ' = ' + contador6 * 6;
+    Tabla6.style.backgroundColor = 'yellow';
+    contador6 ++
+}
+// Tabla del siete
+function TablaDelSiete(){
+
+    Tabla7.innerHTML = '7' + ' X ' + contador7 + ' = ' + contador7 * 7;
+    Tabla7.style.backgroundColor = 'yellow';
+    contador7 ++
+}
+// Tabla del ocho
+function TablaDelOcho(){
+
+    Tabla8.innerHTML = '8' + ' X ' + contador8 + ' = ' + contador8 * 8;
+    Tabla8.style.backgroundColor = 'yellow';
+    contador8 ++
+}
+
+// Tabla del nueve
+function TablaDelNueve(){
+
+    Tabla9.innerHTML = '9' + ' X ' + contador9 + ' = ' + contador9 * 9;
+    Tabla9.style.backgroundColor = 'yellow';
+    contador9 ++
+}
